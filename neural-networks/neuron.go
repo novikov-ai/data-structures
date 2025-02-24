15220f1e5e3fa6b9f1c5933299bf5c36f05e6c11
Alexander Novikov <nvkv_ai@outlook.com>
2025-02-24 22:21:04 +0300

    add learning A

--- neural-networks/neuron.go
+++ neural-networks/neuron.go
@@ -10,6 +10,23 @@ func main() {
 
 }
 
+var dataSet = map[string]rune{
+	"dataset/a1.txt":  'a',
+	"dataset/a2.txt":  'a',
+	"dataset/a3.txt":  'a',
+	"dataset/a4.txt":  'a',
+	"dataset/a5.txt":  'a',
+	"dataset/a6.txt":  'a',
+	"dataset/a7.txt":  'a',
+	"dataset/a8.txt":  'a',
+	"dataset/a9.txt":  'a',
+	"dataset/a10.txt": 'a',
+	"dataset/c.txt":   'c',
+	"dataset/e.txt":   'e',
+	"dataset/l.txt":   'l',
+	"dataset/t.txt":   't',
+}
+
 type Neuron struct {
 	weights   [][]int
 	threshold int
@@ -73,4 +90,70 @@ func (n *Neuron) synapsisCalc(file *os.File) (int, error) {
 
 	println(file.Name(), "count:", hits)
 	return hits, nil
+}
+
+func (n *Neuron) LearnA() error {
+	for path, v := range dataSet {
+		ok, err := n.Activate(path)
+		if err != nil {
+			return err
+		}
+
+		if rune(v) == 'a' {
+			if ok {
+				continue
+			}
+
+			n.IncreaseWeights(path)
+			continue
+		}
+
+		if ok {
+			n.DecreaseWeights(path)
+		}
+	}
+
+	return nil
+}
+
+func (n *Neuron) IncreaseWeights(file string) error {
+	data, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	return n.changeWeight(data, func(value int) int {
+		return value * (1)
+	})
+}
+
+func (n *Neuron) DecreaseWeights(file string) error {
+	data, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	return n.changeWeight(data, func(value int) int {
+		return value * (-1)
+	})
+}
+
+func (n *Neuron) changeWeight(file *os.File, action func(value int) int) error {
+	i := -1
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i++
+		nextLine := scanner.Text()
+		for j, letter := range nextLine {
+			v, err := strconv.Atoi(string(letter))
+			if err != nil {
+				return err
+			}
+
+			n.weights[i][j] += action(v)
+		}
+	}
+
+	return nil
 }
--- neural-networks/neuron_test.go
+++ neural-networks/neuron_test.go
@@ -107,3 +107,94 @@ func Test_Activate(t *testing.T) {
 		})
 	}
 }
+
+func Test_LearnA(t *testing.T) {
+	n := New(10, 10, 35)
+	err := n.LearnA()
+	assert.Nil(t, err)
+
+	tc := []struct {
+		name     string
+		path     string
+		expected bool
+	}{
+		{
+			name:     "A1",
+			path:     "dataset/a1.txt",
+			expected: true,
+		},
+		{
+			name:     "A2",
+			path:     "dataset/a2.txt",
+			expected: true,
+		},
+		{
+			name:     "A3",
+			path:     "dataset/a3.txt",
+			expected: true,
+		},
+		{
+			name:     "A4",
+			path:     "dataset/a4.txt",
+			expected: true,
+		},
+		{
+			name:     "A5",
+			path:     "dataset/a5.txt",
+			expected: true,
+		},
+		{
+			name:     "A6",
+			path:     "dataset/a6.txt",
+			expected: true,
+		},
+		{
+			name:     "A7",
+			path:     "dataset/a7.txt",
+			expected: true,
+		},
+		{
+			name:     "A8",
+			path:     "dataset/a8.txt",
+			expected: true,
+		},
+		{
+			name:     "A9",
+			path:     "dataset/a9.txt",
+			expected: true,
+		},
+		{
+			name:     "A10",
+			path:     "dataset/a10.txt",
+			expected: true,
+		},
+		{
+			name:     "C",
+			path:     "dataset/c.txt",
+			expected: false,
+		},
+		{
+			name:     "E",
+			path:     "dataset/e.txt",
+			expected: false,
+		},
+		{
+			name:     "L",
+			path:     "dataset/l.txt",
+			expected: false,
+		},
+		{
+			name:     "T",
+			path:     "dataset/t.txt",
+			expected: false,
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := n.Activate(tt.path)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
